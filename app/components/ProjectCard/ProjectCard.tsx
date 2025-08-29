7efba87ae019065ffd39bff4c2ef904359695ccb
Prajas Kadepurkar <kadepurkarprajas@gmail.com>
2025-08-28 20:15:40 -0700

    Project Card Visuals

--- app/components/ProjectCard/ProjectCard.tsx
+++ app/components/ProjectCard/ProjectCard.tsx
@@ -13,39 +13,43 @@ const ProjectCard = ({ project }: ProjectCardProps) => {
 			href={`/projects/${project.slug}`}
 			className="block w-full h-full
 			flex-col
-			rounded-xl
-			border-2 border-border-secondary
+			border-2
+			rounded-lg
 			hover:scale-102
 			transition-all ease-out duration-350"
 			style={{
-					borderColor: "var(--project-color-border)",
-				}}
+				borderColor: "var(--project-color-border)",
+			}}
 		>
 			<figure className="w-full aspect-video relative">
 				<Image
-					className="object-cover rounded-t-xl"
+					className="object-cover"
 					src={project.image}
 					alt=""
 					fill
 				/>
 			</figure>
-			<h2
-				className="p-4 mt-4 font-montserrat font-semibold tracking-wide text-text-primary"
-				style={{
-					backgroundColor: "var(--project-color-accent)",
-				}}
-			>
-				{project.description}
+			<h2 className="px-4 mt-4 font-montserrat font-bold tracking-wider text-xl text-text-primary">
+				{project.name}
 			</h2>
+			<h3 className="px-4 py-2 font-nunito font-semibold text-lg tracking-wide text-text-secondary">
+				{project.description}
+			</h3>
 			<ul
 				className="w-full flex flex-wrap gap-2
 				p-4
-				font-nunito text-sm text-text-secondary"
+				font-nunito font-semibold text-xs text-text-secondary uppercase tracking-wider"
 			>
 				{project.tags.map((tag, index, { length }) => {
 					return (
 						<>
-							<li>{tag}</li>
+							<li
+								style={{
+									color: "var(--project-color)",
+								}}
+							>
+								{tag}
+							</li>
 							{index != length - 1 && (
 								<span>&nbsp;&middot;&nbsp;</span>
 							)}
